92b14ebdbe1f755fe25f40f682671d9e30b49136
Алейник Иван <vany05228@gmail.com>
2024-09-19 16:34:54 +0300

    fixed unit test

--- src/demo/parallel/ComplexTest.java
+++ src/demo/parallel/ComplexTest.java
@@ -5,24 +5,16 @@ import org.junit.Assert;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ComplexTest {
-
+    @org.junit.jupiter.api.Test
     void plusTest(){
         Complex testComplex = new Complex(0.5, 0.5);
        testComplex.plus(new Complex(1.3, 2.8));
        Assert.assertEquals(new Complex(1.8, 3.3), testComplex);
     }
-
+    @org.junit.jupiter.api.Test
     void multiplicatTest() {
     Complex testComplex = new Complex(0.5,0.7);
     testComplex.Multiplicat(10);
         Assert.assertEquals(new Complex(5,7), testComplex);
-    }
-
-    @org.junit.jupiter.api.Test
-    void plus() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void multiplicat() {
     }
 }
